59a4bc0a792df904335772d4afa9cef77bb79eff
Ibrahim Hernández <ibrahimhernandezjorge@gmail.com>
2017-03-05 20:17:01 +0000

    Arreglo errores menores

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,14 +3,14 @@ var gulp = require('gulp');
 gulp.task('routes', function() {
     console.log('Hello from routes tasks');
     var exec = require('child_process').exec;
-    var proc = exec("node 1.js", function() {
+    var proc = exec("node 2.js", function() {
     });
 })
 
 gulp.task('express', function() {
     console.log('Hello from express tasks')
     var exec = require('child_process').exec;
-    var proc = exec("node 1.js", function() {
+    var proc = exec("node 3.js", function() {
     });
 })
 
